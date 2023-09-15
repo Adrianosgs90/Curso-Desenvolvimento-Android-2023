774c4698d8e582c38556e461122d5120ccea3b67
AdrianoPC <adrianosgs90@gmail.com>
2023-09-14 22:06:52 -0300

    Criacao classe pessoa camada MODEL 2

--- AppListaCurso/app/src/main/java/devandroid/adriano/applistacurso/view/MainActivity.java
+++ AppListaCurso/app/src/main/java/devandroid/adriano/applistacurso/view/MainActivity.java
@@ -10,6 +10,7 @@ import devandroid.adriano.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    Pessoa outraPessoa;
 
 
     @Override
@@ -24,6 +25,14 @@ public class MainActivity extends AppCompatActivity {
         pessoa.setSobreNome("Maddo");
         pessoa.setCursoDesejado("Android");
         pessoa.setTelefoneDesejado("11-99229955");
+
+        outraPessoa = new Pessoa();
+        outraPessoa.setPrimeiroNome("Luiz");
+        outraPessoa.setSobreNome("Alves");
+        outraPessoa.setCursoDesejado("linguagem");
+        outraPessoa.setTelefoneDesejado("11-123456");
+
+        int parada = 0;
 
 
 
