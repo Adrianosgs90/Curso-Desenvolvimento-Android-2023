5e99488f47f833a6d816d2a71c251324c7166697
Adrianosgs90 <adrianosgs90@gmail.com>
2025-03-24 20:38:31 -0300

    Uso dos métodos gettes e settes

--- app/src/main/java/devandroid/adriano/applistacurso/view/MainActivity.java
+++ app/src/main/java/devandroid/adriano/applistacurso/view/MainActivity.java
@@ -14,6 +14,10 @@ import devandroid.adriano.applistacurso.model.Pessoa;
 public class MainActivity extends AppCompatActivity {
 
     Pessoa pessoa;
+    Pessoa outraPessoa;
+
+    String dadosPessoa;
+    String dadosOutraPessoa;
 
 
     @Override
@@ -30,7 +34,42 @@ public class MainActivity extends AppCompatActivity {
 
         pessoa = new Pessoa();
 
+        // Atribuir conteúdo dados e valoares para o Objeto
+
+        pessoa.setPrimeiroNome("Adriano");
+        pessoa.setSobrenome("Santos");
+        pessoa.setCursoDesejado("Android");
+        pessoa.setTelefoneContato("11-99229191");
+
+        outraPessoa = new Pessoa();
+        outraPessoa.setPrimeiroNome("Luiz");
+        outraPessoa.setSobrenome("Alves");
+        outraPessoa.setCursoDesejado("Java");
+        outraPessoa.setTelefoneContato("11-5599336");
+
+        dadosPessoa = "Primeiro Nome: ";
+        dadosPessoa += pessoa.getPrimeiroNome();
+        dadosPessoa += " Sobrenome: ";
+        dadosPessoa += pessoa.getSobrenome();
+        dadosPessoa += " Curso Desejado: ";
+        dadosPessoa += pessoa.getCursoDesejado();
+        dadosPessoa += " Telefone Contato ";
+        dadosPessoa += pessoa.getTelefoneContato();
+
+        dadosOutraPessoa = "Primeiro Nome: ";
+        dadosOutraPessoa += outraPessoa.getPrimeiroNome();
+        dadosOutraPessoa += " Sobrenome: ";
+        dadosOutraPessoa += outraPessoa.getSobrenome();
+        dadosOutraPessoa += " Curso Desejado: ";
+        dadosOutraPessoa += outraPessoa.getCursoDesejado();
+        dadosOutraPessoa += " Telefone Contato ";
+        dadosOutraPessoa += outraPessoa.getTelefoneContato();
+
+        int parada = 0;
+
+
 
 
     }
+
 }
